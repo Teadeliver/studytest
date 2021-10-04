68078e2965c2934e557530aa5e63a3462b2d23c8
LittleStar <192354134@qq.com>
2021-10-04 23:25:55 +0800

    学习测试

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import sys
-import keyword
-
-
-x = 'runoob'
-sys.stdout.write(x + '\n')
-var = keyword.kwlist
-
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+print("请输入本金：")
+benjin = int(input())
+print("请输入年利率：")
+nianlilv = float(input())
+print("请输入年数：")
+nianshu = int(input())
+i = 0
+print(benjin)
+while i < nianshu:
+    benjin = benjin * (1 + nianlilv / 100)
+    i += 1
+print("本金利率和为：{0；2.2f}", benjin)
+print("本金利率和为：{0；2.2f}", benjin)
