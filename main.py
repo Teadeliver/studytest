24dba913e08a2eb40da535c2375a61ad747f3e8a
LittleStar <192354134@qq.com>
2021-10-08 00:27:37 +0800

    学习测试

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-print("请输入本金：")
-benjin = int(input())
-print("请输入年利率：")
-nianlilv = float(input())
-print("请输入年数：")
-nianshu = int(input())
-i = 0
-print(benjin)
-while i < nianshu:
-    benjin = benjin * (1 + nianlilv / 100)
-    i += 1
-print("本金利率和为：{0；2.2f}", benjin)
-print("本金利率和为：{0；2.2f}", benjin)
+weight = float(input("请输入果子的重量，单位g："))
+if weight < 200:
+    print("小果")
+elif weight < 500:
+    print("中果")
+else:
+    print("大果")
